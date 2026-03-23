e5060613ff4f077aaf2cb906b76f394c6d1252fa
saeederamy <saeederamy@gmail.com>
2026-03-23 23:36:12 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 REPO_URL="https://raw.githubusercontent.com/saeederamy/Self-Web-Host/main"
 
-echo "Installing Black Hub..."
-# دانلود و پاک‌سازی همزمان تمام فایل‌ها
+echo "Cleaning old files..."
+rm -f hub.py manage_hub.sh
+
+echo "Downloading Black Hub..."
 curl -sL "$REPO_URL/hub.py" | tr -d '\r' > hub.py
 curl -sL "$REPO_URL/manage_hub.sh" | tr -d '\r' > manage_hub.sh
 
 chmod +x manage_hub.sh
 
 echo "Installation complete. Starting manager..."
-# اجرای مستقیم منیجر
 ./manage_hub.sh
