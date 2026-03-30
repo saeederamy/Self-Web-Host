a029a937b90c5793f3720cca419aee10404d9143
saeederamy <saeederamy@gmail.com>
2026-03-30 14:58:00 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "------------------------------------------"
 rm -f hub.py manage_hub.sh
 
 # دانلود با حذف کاراکترهای مخفی (\r)
-curl -sL "$REPO_URL/hub.py" | tr -d '\r' > hub.py
+curl -sL "$REPO_URL/hub.py" | tr -d '\r' > hub2.py
 curl -sL "$REPO_URL/manage_hub.sh" | tr -d '\r' > manage_hub.sh
 
 chmod +x manage_hub.sh
